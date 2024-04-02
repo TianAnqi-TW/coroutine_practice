617952c7b283bc4ad9fc06b5184581ebab033e3f
anqi.tian <anqi.tian@thoughtworks.com>
2024-04-02 10:57:19 +0800

    change getplantswithgrowzoneflow function

--- start/src/main/java/com/example/android/advancedcoroutines/PlantRepository.kt
+++ start/src/main/java/com/example/android/advancedcoroutines/PlantRepository.kt
@@ -30,6 +30,7 @@ import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.conflate
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.withContext
 
 /**
@@ -98,8 +99,13 @@ class PlantRepository private constructor(
             }.flowOn(defaultDispatcher)
             .conflate()
 
-    fun getPlantsWithGrowZoneFlow(growZoneNumber: GrowZone): Flow<List<Plant>> {
-        return plantDao.getPlantsWithGrowZoneNumberFlow(growZoneNumber.number)
+    fun getPlantsWithGrowZoneFlow(growZone: GrowZone): Flow<List<Plant>> {
+        return plantDao.getPlantsWithGrowZoneNumberFlow(growZone.number)
+            .map { plantList ->
+                val sortOrderFromNetwork = plantsListSortOrderCache.getOrAwait()
+                val nextValue = plantList.applyMainSafeSort(sortOrderFromNetwork)
+                nextValue
+            }
     }
 
     /**
